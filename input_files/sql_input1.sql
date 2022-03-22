e84cd8b331c00df9d349fac64f0efc1ccc2e5ffd
School <school@kz.lan>
2022-03-21 19:34:55 -0500

    Modified input file

--- input_files/sql_input1.sql
+++ input_files/sql_input1.sql
@@ -3,4 +3,6 @@ FROM finance_sche.book_customer bc, books b, book_order bo, publisher p
 WHERE(publishername = 'PRINTING IS US') and bc.book_customer like 'KZ%';
 SELECT bc.firstname, bc.lastname, b.title, 
 TO_CHAR(bo.orderdate, 'MM/DD/YYYY') "Order Date", p.publishername
-FROM book_customer bc, books b, book_order bo, publisher p WHERE(publishername = 'PRINTING IS US') and bc.book_customer like 'KZ%';
+FROM book_customer bc, books b, book_order, publisher p 
+WHERE
+(publishername = 'PRINTING IS US') and bc.book_customer like 'KZ%' and bc.author_state = 'HI';
--- sql_input.sql
+++
@@ -1,3 +0,0 @@
-        SELECT bc.firstname "First Name", bc.lastname, b.title, TO_CHAR(bo.orderdate, 'MM/DD/YYYY') "Order Date", p.publishername
-FROM finance_sche.book_customer bc, books b, book_order bo, publisher p
-WHERE(publishername = 'PRINTING IS US') and bc.book_customer like 'KZ%';
